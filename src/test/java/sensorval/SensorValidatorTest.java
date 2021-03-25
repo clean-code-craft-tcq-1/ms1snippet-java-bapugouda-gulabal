ff6cccb53c554399ad7183ac051bb3769f9eed13
Bapugouda Gulabal <38274563+bapugouda-gulabal@users.noreply.github.com>
2021-03-25 18:57:53 +0530

    Add files via upload

--- src/test/java/sensorval/SensorValidatorTest.java
+++ src/test/java/sensorval/SensorValidatorTest.java
@@ -1,27 +1,62 @@
 package sensorval;
 
-import static org.junit.Assert.*;
-import jdk.nashorn.internal.AssertsEnabled;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-public class SensorValidatorTest 
-{
-    @Test
-    public void reportsErrorWhenSOCjumps()
-    {
-        Double[] readings = {0.0, 0.01, 0.5, 0.51};
-        List<Double> socs = Arrays.asList(readings);
-        assertFalse(SensorValidator.validateSOCreadings(socs));
-    }
-    @Test
-    public void reportsErrorWhenCurrentjumps()
-    {
-        Double[] readings = {0.03, 0.03, 0.03, 0.33};
-        List<Double> currents = Arrays.asList(readings);
-        assertFalse(SensorValidator.validateCurrentreadings(currents));
-    }
+public class SensorValidatorTest {
+  
+  @Rule
+  public ExpectedException exceptionRule = ExpectedException.none();
+
+  @Test
+  public void reportsErrorWhenSOCjumps() {
+    Double[] readings = { 0.0, 0.01, 0.5, 0.51 };
+    List<Double> socs = Arrays.asList(readings);
+    assertFalse(SensorValidator.validateSOCreadings(socs));
+  }
+  
+  @Test
+  public void reportsErrorWhenSOCReadingIsNull() {
+    exceptionRule.expect(NullPointerException.class);
+    exceptionRule.expectMessage("Please provide proper 'SOC reading' values, Provided value is null!");
+    assertFalse(SensorValidator.validateSOCreadings(null));
+  }
+  
+  @Test
+  public void reportsErrorWhenSOCReadingIsEmpty() {
+    exceptionRule.expect(IllegalArgumentException.class);
+    exceptionRule.expectMessage("Please provide proper 'SOC reading' values, Provided value is empty!");
+    Double[] readings = { };
+    List<Double> socs = Arrays.asList(readings);
+    assertFalse(SensorValidator.validateSOCreadings(socs));
+  }
+  
+  @Test
+  public void reportsErrorWhenCurrentjumps() {
+    Double[] readings = { 0.03, 0.03, 0.03, 0.33 };
+    List<Double> currents = Arrays.asList(readings);
+    assertFalse(SensorValidator.validateCurrentreadings(currents));
+  }
+  
+  @Test
+  public void reportsErrorWhenCurrentReadingIsNull() {
+    exceptionRule.expect(NullPointerException.class);
+    exceptionRule.expectMessage("Please provide proper 'Current reading' values, Provided value is null!");
+    List<Double> currents = null;
+    assertFalse(SensorValidator.validateCurrentreadings(currents));
+  }
+  
+  @Test
+  public void reportsErrorWhenCurrentReadingIsEmpty() {
+    exceptionRule.expect(IllegalArgumentException.class);
+    exceptionRule.expectMessage("Please provide proper 'Current reading' values, Provided value is empty!");
+    List<Double> currents = Arrays.asList();
+    assertFalse(SensorValidator.validateCurrentreadings(currents));
+  }
 }
