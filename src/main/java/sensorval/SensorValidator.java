3afecc46a4f5acacf9c812e91103a1af5ae12e6e
Bapugouda Gulabal <38274563+bapugouda-gulabal@users.noreply.github.com>
2021-04-01 21:40:10 +0530

    Add files via upload

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -7,39 +7,39 @@ import org.apache.commons.lang3.Validate;
 
 public class SensorValidator {
 
-  public static boolean isSubstractionOfNextValueMinusPreviousValueGreaterThanMaxDelta(double value, double nextValue,
-      double maxDelta) {
-    boolean result = Boolean.TRUE;
-    if (nextValue - value > maxDelta) {
-      result = Boolean.FALSE;
-    }
-    return result;
-  }
+  public static boolean isDifferenceGreaterThanThreshold(double value, double nextValue, double maxDelta) {
+	    boolean result = Boolean.TRUE;
+	    if (nextValue - value > maxDelta) {
+	      result = Boolean.FALSE;
+	    }
+	    return result;
+	  }
 
   public static boolean validateSOCreadings(List<Double> values) {
-    Validate.notNull(values, "Please provide proper 'SOC reading' values, Provided value is null!", values);
-    Validate.notEmpty(values, "Please provide proper 'SOC reading' values, Provided value is empty!", values);
-    boolean result = Boolean.TRUE;
-    int lastButOneIndex = values.size() - 1;
-    for (int i = 0; i < lastButOneIndex; i++) {
-      if (!isSubstractionOfNextValueMinusPreviousValueGreaterThanMaxDelta(values.get(i), values.get(i + 1), 0.05)) {
-        result = Boolean.FALSE;
-      }
-    }
-    return result;
-  }
+	    Validate.notNull(values, getValidationMsg("'SOC reading'", null), values);
+	    Validate.notEmpty(values, getValidationMsg("'SOC reading'", "empty"), values);
+	    return validateReadings(values, 0.05);
+	  }
 
-  public static boolean validateCurrentreadings(List<Double> values) {
-    Validate.notNull(values, "Please provide proper 'Current reading' values, Provided value is null!", values);
-    Validate.notEmpty(values, "Please provide proper 'Current reading' values, Provided value is empty!", values);
-    boolean result = Boolean.TRUE;
-    int lastButOneIndex = values.size() - 1;
-    for (int i = 0; i < lastButOneIndex; i++) {
-      if (!isSubstractionOfNextValueMinusPreviousValueGreaterThanMaxDelta(values.get(i), values.get(i + 1), 0.1)) {
-        result = Boolean.FALSE;
-      }
-    }
-    return result;
-  }
+	  public static boolean validateCurrentreadings(List<Double> values) {
+	    Validate.notNull(values, getValidationMsg("'Current reading'", null), values);
+	    Validate.notEmpty(values, getValidationMsg("'Current reading'", "empty"), values);
+	    return validateReadings(values, 0.1);
+	  }
+  
+  private static boolean validateReadings(List<Double> values, double threshold) {
+	    boolean result = Boolean.TRUE;
+	    int lastButOneIndex = values.size() - 1;
+	    for (int i = 0; i < lastButOneIndex; i++) {
+	      if (!isDifferenceGreaterThanThreshold(values.get(i), values.get(i + 1), threshold)) {
+	        result = Boolean.FALSE;
+	      }
+	    }
+	    return result;
+	  }
+  
+  public static String getValidationMsg(String value1, String value2) {
+	    return String.format("Please provide proper %s values, Provided value is %s!", value1, value2);
+	  }
 }
 
